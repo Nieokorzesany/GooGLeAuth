4458bae33fdbbb52c4cacef351f3273fca2c6584
Piotr Wlazeł <pancu1222@gmail.com>
2019-09-27 23:12:02 +0200

    add authentication via google auth

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GOOGLE_CLIENT_ID:
+    "926396638479-cn7ohfsedn0cqbeiqmr86g7ba4nonc8c.apps.googleusercontent.com",
+  GOOGLE_CLIENT_SECRET: "-p4pvLS0jmCRncmAIDXd0Zrg",
+  CALLBACK_URL: "http://localhost:3000/auth/google/callback"
+};
